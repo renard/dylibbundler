6aa62d46520eb01ff925c8b5062431bb7be36783
Auria <auria@users.sourceforge.net>
2012-04-25 15:47:40 +0100

    Imported from dylibbundler0.4.1.zip

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -26,41 +26,41 @@ using namespace std;
 /*
 void setInstallPath(string loc)
 {
-	path_to_libs_folder = loc;
+    path_to_libs_folder = loc;
 }*/
 
 void tokenize(const string& str, const char* delim, vector<string>* vectorarg)
 {
-	vector<string>& tokens = *vectorarg;
-	
-	string delimiters(delim);
-	
-	// skip delimiters at beginning.
-	string::size_type lastPos = str.find_first_not_of( delimiters , 0);
-	
-	// find first "non-delimiter".
-	string::size_type pos = str.find_first_of(delimiters, lastPos);
-	
-	while (string::npos != pos || string::npos != lastPos)
-	{
-		// found a token, add it to the vector.
-		tokens.push_back(str.substr(lastPos, pos - lastPos));
-		
-		// skip delimiters.  Note the "not_of"
-		lastPos = str.find_first_not_of(delimiters, pos);
-		
-		// find next "non-delimiter"
-		pos = str.find_first_of(delimiters, lastPos);
-	}
-	
+    vector<string>& tokens = *vectorarg;
+    
+    string delimiters(delim);
+    
+    // skip delimiters at beginning.
+    string::size_type lastPos = str.find_first_not_of( delimiters , 0);
+    
+    // find first "non-delimiter".
+    string::size_type pos = str.find_first_of(delimiters, lastPos);
+    
+    while (string::npos != pos || string::npos != lastPos)
+    {
+        // found a token, add it to the vector.
+        tokens.push_back(str.substr(lastPos, pos - lastPos));
+        
+        // skip delimiters.  Note the "not_of"
+        lastPos = str.find_first_not_of(delimiters, pos);
+        
+        // find next "non-delimiter"
+        pos = str.find_first_of(delimiters, lastPos);
+    }
+    
 }
 
 
 bool fileExists( std::string filename )
 {
-	//struct stat buffer ;
-	//if ( stat( filename, &buffer ) == 0 ) return true;
-	//return false;
+    //struct stat buffer ;
+    //if ( stat( filename, &buffer ) == 0 ) return true;
+    //return false;
     
     return system( (std::string("ls ")+filename+" > /dev/null").c_str() )==0;
 }
@@ -68,43 +68,43 @@ bool fileExists( std::string filename )
 void fixLibDependency(string old_lib_path, string new_lib_name, string target_file_name)
 {
 
-	string command = string("install_name_tool -change ") + old_lib_path + string(" ") + Settings::inside_lib_path() + new_lib_name + string(" ") + target_file_name;
-	if( systemp( command ) != 0 )
-	{
-		cerr << "\n\nError : An error occured while trying to fix depency of " << old_lib_path << " in " << target_file_name << endl;
-		exit(1);
-	}
+    string command = string("install_name_tool -change ") + old_lib_path + string(" ") + Settings::inside_lib_path() + new_lib_name + string(" ") + target_file_name;
+    if( systemp( command ) != 0 )
+    {
+        cerr << "\n\nError : An error occured while trying to fix depency of " << old_lib_path << " in " << target_file_name << endl;
+        exit(1);
+    }
 }
 
 void copyFile(string from, string to)
 {
-	bool override = Settings::canOverwriteFiles();
-	if(!override)
-	{
-		if(fileExists( to ))
-		{
-			cerr << "\n\nError : File " << to.c_str() << " already exists. Remove it or enable overriding." << endl;
-			exit(1);
-		}
-	}
-	
-	string override_permission = string(override ? "-f " : "-n ");
-		
-	// copy file to local directory
-	string command = string("cp ") + override_permission + from + string(" ") + to;
-	if( systemp( command ) != 0 )
-	{
-		cerr << "\n\nError : An error occured while trying to copy file " << from << " to " << to << endl;
-		exit(1);
-	}
-	
-	// give it write permission
-	string command2 = string("chmod +w ") + to;
-	if( systemp( command2 ) != 0 )
-	{
-		cerr << "\n\nError : An error occured while trying to set write permissions on file " << to << endl;
-		exit(1);
-	}
+    bool override = Settings::canOverwriteFiles();
+    if(!override)
+    {
+        if(fileExists( to ))
+        {
+            cerr << "\n\nError : File " << to.c_str() << " already exists. Remove it or enable overriding." << endl;
+            exit(1);
+        }
+    }
+    
+    string override_permission = string(override ? "-f " : "-n ");
+        
+    // copy file to local directory
+    string command = string("cp ") + override_permission + from + string(" ") + to;
+    if( systemp( command ) != 0 )
+    {
+        cerr << "\n\nError : An error occured while trying to copy file " << from << " to " << to << endl;
+        exit(1);
+    }
+    
+    // give it write permission
+    string command2 = string("chmod +w ") + to;
+    if( systemp( command2 ) != 0 )
+    {
+        cerr << "\n\nError : An error occured while trying to set write permissions on file " << to << endl;
+        exit(1);
+    }
 }
 
 std::string system_get_output(std::string cmd)
